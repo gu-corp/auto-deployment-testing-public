8d2972a145dadfb8ede7e452c7faaee22791ee1e
Andy <andy.ha@vmogroup.com>
2025-01-19 22:29:01 +0700

    fix format

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 # Create a Google Compute instance
 resource "google_compute_instance" "gcp_test_1" {
-  name          = "test-ci"
-  machine_type  = "f1-micro"
-  zone          = "us-east1-b"
-  
+  name         = "test-ci"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
   boot_disk {
     initialize_params {
       image = "ubuntu-minimal-2004-focal-v20250107"
     }
   }
-  
-// Local SSD disk
+
+  // Local SSD disk
   scratch_disk {
     interface = "NVME"
   }
