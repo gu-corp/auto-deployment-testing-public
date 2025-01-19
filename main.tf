60bbbc042905ffaed2bf3951f4a2d321ed2ab92f
Andy <andy.ha@vmogroup.com>
2025-01-19 21:45:51 +0700

    fix image ci

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Create a Google Compute instance
-resource "google_compute_instance" "example" {
+resource "google_compute_instance" "gcp_test_1" {
   name          = "test-ci"
   machine_type  = "f1-micro"
   zone          = "us-east1-b"
   
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+      image = "ubuntu-minimal-2004-focal-v20250107"
     }
   }
   
