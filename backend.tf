7bb059763da019799b2bce7d8aaa88dcbc99013f
Andy <andy.ha@vmogroup.com>
2025-01-19 21:34:50 +0700

    fix terraform backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,6 @@
-# Define Terraform backend using a Google Cloud Storage bucket for storing the Terraform state
 terraform {
   backend "gcs" {
-    bucket  = "terraform-state-github-action-cicd"
-    path    = "terraform/develop.tfstate"
-    project = "gu-testing-workspace"
- }
-}
+    bucket = "terraform-state-github-action-cicd"
+    prefix = "terraform/develop"
+  }
+} 
