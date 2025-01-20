6a20700cf48d80ce1797c48b1f82fbcf4909a790
Andy <andy.ha@vmogroup.com>
2025-01-20 11:00:56 +0700

    add stacks

--- stacks/compute-engine/instance-2/main.tf
+++ stacks/compute-engine/instance-2/main.tf
@@ -1,35 +1,4 @@
 # Create a Google Compute instance
-resource "google_compute_instance" "gcp_test_1" {
-  name         = "test-ci-1"
-  machine_type = "f1-micro"
-  zone         = "us-east1-b"
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-minimal-2004-focal-v20250107"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-}
-
 
 resource "google_compute_instance" "gcp_test_2" {
   name         = "test-ci-2"
